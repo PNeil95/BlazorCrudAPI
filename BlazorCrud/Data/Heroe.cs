0e095192e8ed267328196d18f6ed6f77c8924c40
PNeil95 <pvbn9518@gmail.com>
2021-11-12 21:23:13 -0500

    Iniciando

---
+++ BlazorCrud/Data/Heroe.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCrud.Data
+{
+    public class Heroe
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Poder { get; set; }
+        public string Universo { get; set; }
+    }
+}
